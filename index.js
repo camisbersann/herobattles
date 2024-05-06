5b7ffbb2687fdf5f8ed0c012a86e1d58b58705b8
camilabersan <camilabersan.564@gmail.com>
2024-05-06 09:28:27 -0300

    feat: create get all heroes route

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ app.get('/', (req, res) => {
     res.send('A rota está funcionado!')
 });
 
+app.get('/heroes', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM heroes');
+        res.json({
+            total: result.rowCount,
+            heroes: result.rows,
+        });
+    } catch (error) {
+       console.error('Erro ao obter todos os heróis', error); 
+       res.status(500).send('Erro ao obter todos os heróis');
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
