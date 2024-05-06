1133173451f61d8609eea3d2b2770990f319dba7
camilabersan <camilabersan.564@gmail.com>
2024-05-06 09:18:29 -0300

    feat: conection with db

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const PORT= 4000;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'herobattles',
+    password: 'ds564',
+    port: 7007,
+});
+
+app.use(express.json());
